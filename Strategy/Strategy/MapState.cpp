ac4973c08eeab1ede67bbebb099b5d3f0779aa94
desk2000 <52657198+desk2000@users.noreply.github.com>
2020-09-03 23:59:45 +0800

    Update .gitignore

--- Strategy/Strategy/MapState.cpp
+++ Strategy/Strategy/MapState.cpp
@@ -80,6 +80,7 @@ void MapState::initPoints()
 	points.emplace_back(1959.78f, 2633.9f, L"星巴克 公館店", shopIndex++, textures["locationMark"], &font);
 	//player->setPosition(&points.at(player->getID() - 1));
 
+	// hi
 	std::vector<std::vector<sf::Uint8>> tagsShopIndex =
 	{
 		{ 13, 14, 15, 21, 22, 24, 25, 26, 28 },
